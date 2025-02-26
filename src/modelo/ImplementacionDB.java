621ba7894ffc10e1f6b2ae63e67c79b10f36dff4
Jago128 <jagobaetxegarai@gmail.com>
2025-02-26 10:22:11 +0100

    e

--- src/modelo/ImplementacionDB.java
+++ src/modelo/ImplementacionDB.java
@@ -11,6 +11,8 @@ public class ImplementacionDB {
 	private String urlDB;
 	private String userDB;
 	private String passwordDB;
+	
+	final String SQL="SELECT * FROM usuario WHERE usuario=? AND contraseña=?";
 
 	public ImplementacionDB() {
 		this.configFile=ResourceBundle.getBundle("configClase");
@@ -24,11 +26,33 @@ public class ImplementacionDB {
 		try {
 			con=DriverManager.getConnection(urlDB, this.userDB, this.passwordDB);
 		} catch (SQLException e) {
-			System.out.println("Error al intentar abrir la BD");
+			System.out.println("Error al intentar abrir la base de datos.");
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+	}
+
+	public boolean comprobarUsuario(Usuario user) {
+		boolean existe=false;
+		this.openConnection();
+
+		try {
+			stmt=con.prepareStatement(SQL);
+			stmt.setString(1, user.getNombre());
+			stmt.setString(2, user.getContraseña());
+			ResultSet resultado=stmt.executeQuery();
+
+			if (resultado.next()) {
+				existe=true;
+			}
+			
+			resultado.close();
+			stmt.close();
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("Error al verificar credenciales: "+e.getMessage());
+		}
+		return existe;
 	}
 }
