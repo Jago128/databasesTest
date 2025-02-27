752f4c02b352c1b5d2ea7c7a7d3589169e4fb477
Jago128 <jagobaetxegarai@gmail.com>
2025-02-27 10:42:07 +0100

    e

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.*;
+
 import modelo.*;
 import utilidades.Utilidades;
 
@@ -13,33 +15,37 @@ public class Main {
 		name=Utilidades.introducirCadena();
 		System.out.println("Introduce la contraseña:");
 		password=Utilidades.introducirCadena();
-		System.out.println(i.comprobarUsuario(new Usuario(name,password)));
-		
-		do {
-			menu=menu();
-			switch (menu) {
-			case 1:
-				
-				break;
-				
-			case 2:
-				
-				break;
-				
-			case 3:
-				
-				break;
-				
-			case 4:
-				
-				break;
-				
-			case 0:
-				System.out.println("Adios!");
-			}
-		} while (menu!=0);
+
+		if (i.comprobarUsuario(new Usuario(name,password))) {
+			do {
+				menu=menu();
+				switch (menu) {
+				case 1:
+					insert(i);
+					break;
+
+				case 2:
+					update(i);
+					break;
+
+				case 3:
+					delete(i);
+					break;
+
+				case 4:
+					show(i);
+					break;
+
+				case 0:
+					System.out.println("Adios!");
+				}
+			} while (menu!=0);
+		} else {
+			System.out.println("Las credenciales introducidas son incorrectas.");
+		}
+
 	}
-	
+
 	public static int menu() {
 		System.out.println("1. Insertar");
 		System.out.println("2. Modificar");
@@ -47,5 +53,48 @@ public class Main {
 		System.out.println("4. Mostrar");
 		System.out.println("0. Salir");
 		return Utilidades.leerInt("Elija una opcion:",0,4);
+	}
+	
+	public static void insert(ImplementacionDB i) {
+		String nom, pass;
+		System.out.println("Introduce el nombre:");
+		nom=Utilidades.introducirCadena();
+		System.out.println("Introduce la contraseña:");
+		pass=Utilidades.introducirCadena();
+		if (i.insert(new Usuario(nom,pass))) {
+			System.out.println("El usuario ha sido introducido correctamente.");
+		} else {
+			System.out.println("Error al introducir el usuario.");
+		}
+	}
+	
+	public static void update(ImplementacionDB i) {
+		String nom, pass;
+		System.out.println("Introduce el nombre del usuario a modificar:");
+		nom=Utilidades.introducirCadena();
+		System.out.println("Introduce la nueva contraseña:");
+		pass=Utilidades.introducirCadena();
+		if (i.update(nom, pass)) {
+			System.out.println("El usuario ha sido actualizado correctamente.");
+		} else {
+			System.out.println("Error al actualizar el usuario.");
+		}
+	}
+	
+	public static void delete(ImplementacionDB i) {
+		System.out.println("Introduce el nombre del usuario a eliminar:");
+		if (i.delete(Utilidades.introducirCadena())) {
+			System.out.println("El usuario ha sido eliminado correctamente.");
+		} else {
+			System.out.println("Error al eliminar el usuario.");
+		}
+	}
+
+	public static void show(ImplementacionDB i) {
+		Map<String, Usuario> users=new TreeMap<>();
+		users=i.consultaUsuarios();
+		for (Usuario user:users.values()) {
+			System.out.println(user.toString());
+		}
 	}
 }
--- src/modelo/ImplementacionDB.java
+++ src/modelo/ImplementacionDB.java
@@ -7,13 +7,18 @@ public class ImplementacionDB {
 	private Connection con;
 	private PreparedStatement stmt;
 	private ResourceBundle configFile;
+	@SuppressWarnings("unused")
 	private String driverDB;
 	private String urlDB;
 	private String userDB;
 	private String passwordDB;
-	
-	final String SQL="SELECT * FROM usuario WHERE nombre=? AND contraseña=?";
 
+	final String SQL="SELECT * FROM usuario WHERE nombre=? AND contraseña=?";
+	final String INSERTUSER="INSERT INTO usuario VALUES (?,?)";
+	final String UPDATEUSER="UPDATE usuario SET password=? WHERE nombre=?";
+	final String DELETEUSER="DELETE FROM usuario WHERE nombre=?";
+	final String SHOWALL="SELECT * FROM usuario";
+	
 	public ImplementacionDB() {
 		this.configFile=ResourceBundle.getBundle("configClase");
 		this.driverDB=this.configFile.getString("Driver");
@@ -35,24 +40,101 @@ public class ImplementacionDB {
 
 	public boolean comprobarUsuario(Usuario user) {
 		boolean existe=false;
-		this.openConnection();
 
+		this.openConnection();
 		try {
 			stmt=con.prepareStatement(SQL);
 			stmt.setString(1, user.getNombre());
 			stmt.setString(2, user.getContraseña());
-			ResultSet resultado=stmt.executeQuery();
-
-			if (resultado.next()) {
+			ResultSet rs=stmt.executeQuery();
+			if (rs.next()) {
 				existe=true;
 			}
-			
-			resultado.close();
+			rs.close();
 			stmt.close();
 			con.close();
 		} catch (SQLException e) {
 			System.out.println("Error al verificar credenciales: "+e.getMessage());
 		}
 		return existe;
+	}
+
+	public boolean insert(Usuario user) {	
+		boolean check=false;
+
+		this.openConnection();
+		if (!comprobarUsuario(user)) {
+			try {
+				stmt=con.prepareStatement(INSERTUSER);
+				stmt.setString(1, user.getNombre());
+				stmt.setString(2, user.getContraseña());
+				if (stmt.executeUpdate()>0) {
+					check=true;
+				}
+				stmt.close();
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Error al verificar credenciales: " + e.getMessage());
+			}
+		}
+		return check;
+	}
+	
+	public boolean update(String nom, String pass) {
+		boolean check=false;
+		
+		try {
+			stmt=con.prepareStatement(UPDATEUSER);
+			stmt.setString(1, pass);
+			stmt.setString(2, nom);
+			if (stmt.executeUpdate()>0) {
+				check=true;
+			}
+			stmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return check;
+	}
+	
+	public boolean delete(String nom) {
+		boolean check=false;
+		
+		try {
+			stmt=con.prepareStatement(DELETEUSER);
+			stmt.setString(1, nom);
+			if (stmt.executeUpdate()>0) {
+				check=true;
+			}
+			stmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return check;
+	}
+
+	public Map<String, Usuario> consultaUsuarios() {
+		Map<String, Usuario> users=new TreeMap<>();
+		Usuario user;
+		ResultSet rs=null;
+		this.openConnection();
+		try {
+			stmt=con.prepareStatement(SHOWALL);
+			rs=stmt.executeQuery();
+			while (rs.next()) {
+				user=new Usuario();
+				user.setNombre(rs.getString("nombre"));
+				user.setContraseña("contraseña");
+				users.put(user.getNombre(), user);
+			}
+			rs.close();
+			stmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return users;
 	}
 }
